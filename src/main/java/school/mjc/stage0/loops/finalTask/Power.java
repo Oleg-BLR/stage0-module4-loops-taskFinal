5a6b13aede3058ada5d55da1407e5bfa47637e58
Oleg <Oleg-BLR@yandex.ru>
2022-10-30 10:40:56 +0300

    solution 01

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int resultPower = 1;
+        for (int i = 1; i <= power; i++) {
+            resultPower = resultPower * numberToPrint;
+        }
+        System.out.println(resultPower);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,11 +1,37 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
-    public void printPyramid(int cathetusLength) {
-
-    }
-
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        printPyramid(9);
     }
+    public static void printPyramid(int cathetusLength) {
+        for (int i = cathetusLength; i >= 1; i--) {
+            for (int j = (cathetusLength * -1) + 1; j <= cathetusLength -1; j++) {
+                int elementIJ = 1;
+                if (j < 0) {
+                    if (j < i - cathetusLength) {
+                        System.out.print(" ");
+                    } else {
+                        elementIJ = -(j - 1);
+                        System.out.print(elementIJ);
+                    }
+                } else if (j > 0) {
+                    if (j > cathetusLength - i) {
+                        System.out.print(" ");
+
+                    } else {
+                        elementIJ = j + 1;
+                        System.out.print(elementIJ);
+                    }
+                } else {
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+//    public static void main(String[] args) {
+//        new Pyramid().printPyramid(7);
+//    }
 }
